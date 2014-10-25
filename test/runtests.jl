dc5e52165be697e8f3e9103085286f45aaffe49c
Jared Kofron <jared.kofron@gmail.com>
2014-10-25 13:59:34 -0700

    run QF QA test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using Estimation
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+sig = [cos(2*pi*0.15*i) for i=1:2048];
+@test_approx_eq_eps Estimation.quinn_fernandes(sig,2*pi*0.1)/(2*pi) 0.15 1e-5
